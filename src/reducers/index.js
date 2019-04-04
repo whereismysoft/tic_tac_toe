b065b2a0489248dc963a22b71dcabb6b67591f98
whereismysoft <kotovyuriy27@gmail.com>
2019-04-04 14:54:16 +0300

    added app connected to redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,13 @@
+import * as constants from '../constants';
+
 const initialState = {
-    ...data
+    data: []
+}
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+
 }
