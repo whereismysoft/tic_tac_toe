82f4c9a676b3148898bfa370a88489df121c9028
whereismysoft <kotovyuriy27@gmail.com>
2019-04-05 14:10:40 +0300

    rendered example component

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import ExampleComponent from './components/ExampleComponent';
 
 export default class App extends Component {
     render() {
         return (
             <div>
                Hello World ^.^
+               <ExampleComponent />
             </div>
         )
     }
