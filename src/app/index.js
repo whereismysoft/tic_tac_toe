58147c0e5c5b5248cf931a47e8155ab0c97404db
Yury <kotovyuriy27@gmail.com>
2020-01-16 16:27:02 +0300

    init

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,92 @@
 import React, { Component } from 'react';
 import ExampleComponent from './components/ExampleComponent';
 
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-               <ExampleComponent />
-            </div>
-        )
+const fieldSize = [10, 5]
+
+export default class App extends React.Component {
+    // осуществляем проверку (на колличество подряд идущих одинаковы элементов равно числу, означающее выигращную последовательность)
+    // только после хода (ход -> проверка -> количестов подряд идущих символов === n ? выиграл : не выиграл)
+    // начало проверки - текущий ход
+  state = {
+    fields: [],
+    cellSize: {
+      width: window.innerWidth/fieldSize[0],
+      height: window.innerHeight/fieldSize[1]
     }
+  }
+  
+  componentDidMount() {
+    // create multidimensional array with empty elements
+    const [width, height] = fieldSize;
+    this.setState({
+      fields: this.generateEmptyArray(height, width)
+    })
+    document.addEventListener("mousewheel", this.onScroll, false);
+  }
+
+  generateEmptyArray = (height, width) => {
+    return Array.from(
+         new Array(Math.floor(height)), () => Array.from(new Array(Math.floor(width)), () => false)
+      )
+  }
+
+  handleCellClick = e => {
+    const element = e.target;
+    if (element.tagName.toLowerCase() !== 'td') return;
+    console.dir(element.dataset.index);
+  }
+  
+  onScroll = (e) => {
+    console.log(e)
+  }
+  
+  render() {
+    const {
+      fields,
+      cellSize: { width, height }
+    } = this.state;
+    console.log('', this.state);
+    return(
+      <table className="field">
+        <tbody onClick={this.handleCellClick}>
+          {fields.map( 
+            (rows, rowIdx) => (
+              <tr
+                className="row"
+                style={{height: height + 'px'}}
+                key={rowIdx}
+               >
+                {rows.map(
+                  (value, cellIdx) => {
+                    const param = rowIdx + '_' + cellIdx;
+                    return (
+                      <td
+                        className="cell"
+                        style={{width: width + 'px'}}
+                        data-index={param}
+                        key={param}
+                       >
+                        {value ? value : ''}
+                      </td>
+                  )}
+                )}
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+    );
+  }
 }
+
+
+
+// export default class App extends Component {
+//     render() {
+//         return (
+//             <div>
+//                <ExampleComponent />
+//             </div>
+//         )
+//     }
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const CONFIGURATION_VARIABLES = require('./configuration/variables')
 
-console.log('[CONFIGURATION_VARIABLES]', CONFIGURATION_VARIABLES);
-
 module.exports = {
     // webpack 4 takes '/src/index.js' by default
     // entry: path.join(__dirname, '/src/index.js'),
@@ -16,7 +14,11 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 options: {
-                    presets: ['@babel/preset-env', "@babel/preset-react"]
+                    presets: [
+                        "@babel/preset-env",
+                        "@babel/preset-react",
+                        "@babel/plugin-proposal-class-properties",
+                    ]
                 }
             },
             {
